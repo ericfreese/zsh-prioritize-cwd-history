0d2317a5ff157e451083e264f5bcbdfa1a36147f
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-03 11:29:33 -0700

    No clobber ZSH_PRIORITIZE_CWD_HISTORY_DIR w/source

--- src/config.zsh
+++ src/config.zsh
@@ -3,4 +3,7 @@
 # Global Config Variables                                            #
 #--------------------------------------------------------------------#
 
-ZSH_PRIORITIZE_CWD_HISTORY_DIR="$HOME/.zsh_prioritize_cwd_history"
+if [ -z "$ZSH_PRIORITIZE_CWD_HISTORY_DIR" ];then
+	# only override if there's no variable
+	ZSH_PRIORITIZE_CWD_HISTORY_DIR="$HOME/.zsh_prioritize_cwd_history"
+fi
